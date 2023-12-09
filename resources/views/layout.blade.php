a87193104ebe5b569961ee5fe8f31b5b24629d8f
HawKeRian <honong2350@gmail.com>
2023-12-09 14:12:04 +0700

    update 1.0.4

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Web Monitor Template</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
@@ -14,5 +16,6 @@
 
     @yield('content')
     
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 </body>
 </html>
